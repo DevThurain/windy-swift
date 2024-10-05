c5887d700295787e9d51744d121afc6197aa761e
Thurain <thurainbowin.dev@gmail.com>
2024-10-05 15:38:10 +0630

    Initial Commit

---
+++ Windy/WindyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindyApp.swift
+//  Windy
+//
+//  Created by Thurain on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WindyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
